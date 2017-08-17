745d0cebe4d4f7aea6840b23661a33686b66fe1b
aMondelin <anthonydu77120@hotmail.fr>
2017-08-17 14:17:05 +0200

    Nomenclature

--- projectmanager/__init__.py
+++ projectmanager/__init__.py
@@ -1,38 +1,100 @@
 import os
 
-CHARACTERS_ROOT = "D:/ProjectManager-Template/assets/characters/"
-PROPS_ROOT = "D:/ProjectManager-Template/assets/props/"
-ENVIRONMENTS_ROOT = "D:/ProjectManager-Template/assets/environments/"
-SHOT_ROOT = "D:/ProjectManager-Template/shots/"
+PROJECT_ROOT = "D:/ProjectManager-Template"
+ASSET_FOLDER = "assets"
+SHOT_FOLDER = "shots"
+WIP_FOLDER = "wip"
 
 
 def _list_folders(root):
     folders = list()
-    root_items = os.listdir(root)
+    try:
+        root_items = os.listdir(root)
+    except WindowsError:
+        return list()
 
     for item in root_items:
-        if os.path.isdir(root + item):
+        if os.path.isdir(os.path.join(root, item)):
             folders.append(item)
 
     return folders
 
 
-def all_tasks(character = ""):
-    pickFolder = CHARACTERS_ROOT + character + "/"
-    return _list_folders(str(pickFolder))
+def _list_files(root):
+    files = list()
+    root_items = os.listdir(root)
+
+    for item in root_items:
+        if os.path.isfile(os.path.join(root, item)):
+            files.append(item)
+
+    return files
 
 
-def all_characters():
-    return _list_folders(CHARACTERS_ROOT)
+def all_asset_types():
+    assets_folder = os.path.join(
+        PROJECT_ROOT,
+        ASSET_FOLDER
+    )
+    return _list_folders(assets_folder)
 
 
-def all_props():
-    return _list_folders(PROPS_ROOT)
+def all_assets(asset_type):
+    asset_types_folder = os.path.join(
+        PROJECT_ROOT,
+        ASSET_FOLDER,
+        asset_type
+    )
+    return _list_folders(asset_types_folder)
 
 
-def all_environments():
-    return _list_folders(ENVIRONMENTS_ROOT)
+def all_tasks(asset_type, asset_name):
+    asset_folder = os.path.join(
+        PROJECT_ROOT,
+        ASSET_FOLDER,
+        asset_type,
+        asset_name
+    )
+    return _list_folders(asset_folder)
+
+
+def last_version(asset_type, asset_name, task_name):
+    versions_path = os.path.join(
+        PROJECT_ROOT,
+        ASSET_FOLDER,
+        asset_type,
+        asset_name,
+        task_name,
+        WIP_FOLDER
+    )
+    versions = _list_files(versions_path)
+    return sorted(versions)[-1]
 
 
 def all_shots():
-    return _list_folders(SHOT_ROOT)
+    shot_folder = os.path.join(
+        PROJECT_ROOT,
+        SHOT_FOLDER
+    )
+    return _list_folders(shot_folder)
+
+
+def all_shot_tasks(shot_number):
+    shot_task_folder = os.path.join(
+        PROJECT_ROOT,
+        SHOT_FOLDER,
+        shot_number
+    )
+    return _list_folders(shot_task_folder)
+
+
+def last_shot_version(shot_number, task_name):
+    shot_version_path = os.path.join(
+        PROJECT_ROOT,
+        SHOT_FOLDER,
+        shot_number,
+        task_name,
+        WIP_FOLDER
+    )
+    versions = _list_files(shot_version_path)
+    return sorted(versions)[-1]
--- test.py
+++ test.py
@@ -1,16 +1,31 @@
 import projectmanager
 
+#
+# User Story #1 : ETQ graphiste, je souhaite trouver la derniere version de mon travail
 
-characters = projectmanager.all_characters()
-props = projectmanager.all_props()
-environments = projectmanager.all_environments()
+# Choix "Asset"
+asset_types = projectmanager.all_asset_types()
+
+# Choix "Characters"
+characters = projectmanager.all_assets(asset_type="characters")
+
+# Choix "Pierre"
+asset_tasks = projectmanager.all_tasks(asset_type="characters", asset_name="pierre")
+
+# Choix "Modeling"
+last_asset_version = projectmanager.last_version(asset_type="characters", asset_name="pierre", task_name="modeling")
+
+# Choix "Shot"
 shots = projectmanager.all_shots()
 
-getCharacter = "pierre"
-tasks = projectmanager.all_tasks(getCharacter)
+# Choix "Task"
+shot_tasks = projectmanager.all_shot_tasks(shot_number="p001")
 
-print "\n".join(tasks)
+# Choix "Dernier Shot"
+last_shot_version = projectmanager.last_shot_version(shot_number="p001", task_name="animation")
+print last_shot_version
 
-
-# print "\n".join(characters)
-# print "\n".join(shots)
+#
+# User Story #2 : ETQ Graphiste, je souhaite pouvoir ajouter un asset facilement
+projectmanager.create_asset(type_name="characters", name="jean")
+projectmanager.create_tasks(type_name="characters", name="jean", task_name=["modeling", "rig"])
